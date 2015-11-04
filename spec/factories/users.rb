0cdcb5c4c9cc00d689b4416aed812ff3f87ed82c
Erin Isbell <erinmarie.isbell@gmail.com>
2015-11-04 10:37:59 -0600

    Added Travis

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     # username { Faker::Internet.user_name }
     email { Faker::Internet.email }
-   password_digest { Faker::Internet.password }
+   password { Faker::Internet.password }
   end
 
 end

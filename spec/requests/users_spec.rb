95d063f19549b79dbb3a749044e12b86fe8a0d74
Erin Isbell <erinmarie.isbell@gmail.com>
2015-11-03 18:29:50 -0600

    Passing Four tests in user

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,59 +5,52 @@ RSpec.describe "Users" do
 
 
   describe "#create" do
-      it "creates user" do
-        payload = {
-          user: {
-            username: "What",
-            email: "faker@fake.com",
-            password: "password"
-          }
+    it "creates user" do
+      payload = {
+        user: {
+          username: "What",
+          email: "faker@fake.com",
+          password: "password"
         }
-        post users_path, payload
-        expect(response).to have_http_status(:created)
-      end
-
-      it "requires email" do
-        payload = {
-          user: {
-            username: "What",
-            email: "",
-            password: "dowhatnow"
-          }
-        }
-        post users_path, payload
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(json["email"]).to_not be_empty
-      end
+      }
+      post users_path, payload
+      expect(response).to have_http_status(:created)
     end
 
-
-    describe "#index" do
-      it "shows multiple users" do
-        payload = {
-          user: {
-            username: "What",
-            email: "faker@fake.com",
-            password: "password"
-          }
+    it "requires email" do
+      payload = {
+        user: {
+          username: "What",
+          email: "",
+          password: "dowhatnow"
         }
-        get users_path(payload)
-        expect(response).to have_http_status(:success)
-
-      end
-    end
-
-    describe "#show" do
-      it "shows a user " do
-        payload = {
-          user: {
-            username: "What",
-            email: "faker@fake.com"
-          }
-        }
-        get users_path(payload)
-        expect(response).to have_http_status(:success)
-        expect(json[""].count).to eq 1
-      end
+      }
+      post users_path, payload
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(json["email"]).to_not be_empty
     end
   end
+
+
+  describe "#index" do
+    let(:user1) { FactoryGirl.create :user }
+    it "shows multiple users" do
+      user1
+      get users_path
+      expect(response).to have_http_status(:success)
+      expect(json.count).to eq 1
+    end
+  end
+
+  describe "#show" do
+    let(:user1) { FactoryGirl.create :user }
+    it "shows a user" do
+      get users_path, user1
+      expect(response).to have_http_status(:success)
+      expect(json[0]["username"]).to eq user1["username"]
+    end
+  end
+
+
+
+end
